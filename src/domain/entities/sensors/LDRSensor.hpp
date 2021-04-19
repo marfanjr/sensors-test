1601561af32af38d58ffae977444c2f68b5e704b
Murilo Oyafuso <muoyafuso@gmail.com>
2021-04-19 11:30:59 -0300

    set ldr pin

--- src/domain/entities/sensors/LDRSensor.hpp
+++ src/domain/entities/sensors/LDRSensor.hpp
@@ -5,7 +5,7 @@
 
 class LDRSensor : public ISensor{
     private:
-    static const uint8_t PIN = 2;
+    static const uint8_t PIN = 0;
     float getLux();
     
     public:
@@ -20,7 +20,7 @@ class LDRSensor : public ISensor{
 
 float LDRSensor::getLux() {
     uint16_t _digitalValue = analogRead(LDRSensor::PIN);
-    float _analogValue = map (_digitalValue, 0, 1024, 0.0, 1.0);
+    float _analogValue = _digitalValue/1023.00;
     return _analogValue;
 }
 
